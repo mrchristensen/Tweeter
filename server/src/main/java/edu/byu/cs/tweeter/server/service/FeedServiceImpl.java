075db6eac2e72d56611aa0bb232175010b93a915
Matt Christensen <mr.christensen@gmail.com>
2020-03-18 15:24:43 -0600

    All follow related tasks are now delegated to the Follow DAO (centralization)

--- server/src/main/java/edu/byu/cs/tweeter/server/service/FeedServiceImpl.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/FeedServiceImpl.java
@@ -1,15 +1,9 @@
 package edu.byu.cs.tweeter.server.service;
 
-import java.io.IOException;
-
 import edu.byu.cs.tweeter.server.dao.FeedDAO;
-import edu.byu.cs.tweeter.server.dao.FollowerDAO;
 import edu.byu.cs.tweeter.shared.model.service.FeedService;
-import edu.byu.cs.tweeter.shared.model.service.FollowerService;
 import edu.byu.cs.tweeter.shared.model.service.request.FeedRequest;
-import edu.byu.cs.tweeter.shared.model.service.request.FollowerRequest;
 import edu.byu.cs.tweeter.shared.model.service.response.FeedResponse;
-import edu.byu.cs.tweeter.shared.model.service.response.FollowerResponse;
 
 /**
  * Contains the business logic for getting the users a user is following.
