1b3a407770812396c0d0b5b37a0001dbe682a152
Matt Christensen <mr.christensen@gmail.com>
2020-03-22 16:41:05 -0600

    Refactored SessionCache to include the cached AuthToken

--- app/src/main/java/edu/byu/cs/tweeter/model/services/RegisterServiceProxy.java
+++ app/src/main/java/edu/byu/cs/tweeter/model/services/RegisterServiceProxy.java
@@ -3,12 +3,8 @@ package edu.byu.cs.tweeter.model.services;
 import java.io.IOException;
 
 import edu.byu.cs.tweeter.net.ServerFacade;
-import edu.byu.cs.tweeter.shared.model.domain.User;
-import edu.byu.cs.tweeter.shared.model.service.LoginService;
 import edu.byu.cs.tweeter.shared.model.service.RegisterService;
-import edu.byu.cs.tweeter.shared.model.service.request.LoginRequest;
 import edu.byu.cs.tweeter.shared.model.service.request.RegisterRequest;
-import edu.byu.cs.tweeter.shared.model.service.response.LoginResponse;
 import edu.byu.cs.tweeter.shared.model.service.response.RegisterResponse;
 
 /**
@@ -23,10 +19,6 @@ public class RegisterServiceProxy implements RegisterService {
     @Override
     public RegisterResponse getRegister(RegisterRequest request) throws IOException {
         RegisterResponse response = serverFacade.getRegister(request, URL_PATH);
-
-        if(response.isRegisterSuccessful()){
-            CurrentUserService.getInstance().setCurrentUser(response.getCurrentUser());
-        }
 
         return response;
     }
--- app/src/main/java/edu/byu/cs/tweeter/net/SessionCache.java
+++ app/src/main/java/edu/byu/cs/tweeter/net/SessionCache.java
@@ -1,26 +1,28 @@
-package edu.byu.cs.tweeter.model.services;
+package edu.byu.cs.tweeter.net;
 
 import edu.byu.cs.tweeter.shared.model.domain.User;
 
-public class CurrentUserService {
+public class SessionCache {
     /**
      * The singleton instance.
      */
-    private static CurrentUserService instance;
+    private static SessionCache instance;
 
     /**
      * The logged in user.
      */
     private User currentUser;
 
+    private String authTokenString;
+
     /**
      * Return the singleton instance of this class.
      *
      * @return the instance.
      */
-    public static CurrentUserService getInstance() {
+    public static SessionCache getInstance() {
         if(instance == null) {
-            instance = new CurrentUserService();
+            instance = new SessionCache();
         }
 
         return instance;
@@ -30,7 +32,7 @@ public class CurrentUserService {
      * A private constructor created to ensure that this class is a singleton (i.e. that it
      * cannot be instantiated by external classes).
      */
-    private CurrentUserService() {
+    private SessionCache() {
     }
 
     /**
@@ -44,5 +46,17 @@ public class CurrentUserService {
 
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
+    }
+
+    public static void setInstance(SessionCache instance) {
+        SessionCache.instance = instance;
+    }
+
+    public String getAuthTokenString() {
+        return authTokenString;
+    }
+
+    public void setAuthTokenString(String authTokenString) {
+        this.authTokenString = authTokenString;
     }
 }
