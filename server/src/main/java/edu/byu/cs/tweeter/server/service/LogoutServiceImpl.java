b0ccdc7cf9bab05fa7561b67ae2817dfbf032b78
Matt <mr.christensen@gmail.com>
2020-04-13 01:36:23 -0600

    Refactored LogoutDAO into AuthTokenDAO

--- server/src/main/java/edu/byu/cs/tweeter/server/service/LogoutServiceImpl.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/LogoutServiceImpl.java
@@ -1,8 +1,6 @@
 package edu.byu.cs.tweeter.server.service;
 
 import edu.byu.cs.tweeter.server.dao.AuthTokenDAO;
-import edu.byu.cs.tweeter.server.dao.LogoutDAO;
-import edu.byu.cs.tweeter.shared.model.domain.AuthToken;
 import edu.byu.cs.tweeter.shared.model.service.LogoutService;
 import edu.byu.cs.tweeter.shared.model.service.request.LogoutRequest;
 import edu.byu.cs.tweeter.shared.model.service.response.LogoutResponse;
@@ -14,9 +12,6 @@ public class LogoutServiceImpl implements LogoutService {
     @Override
     public LogoutResponse doLogout(LogoutRequest request) {
         AuthTokenDAO authTokenDAO = new AuthTokenDAO();
-        authTokenDAO.invalidateAuthToken(new AuthToken(request.getAuthTokenString()));
-
-        LogoutDAO logoutDAO = new LogoutDAO();
-        return logoutDAO.doLogout(request);
+        return new LogoutResponse(authTokenDAO.deleteAuthToken(request.getAuthTokenString()), request.getCurrentUser());
     }
 }
