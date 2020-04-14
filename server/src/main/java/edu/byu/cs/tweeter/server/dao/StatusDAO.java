4f702253f57becf8d6c7bd17e7444dfcb38163bf
Matt <mr.christensen@gmail.com>
2020-04-14 13:43:20 -0600

    Fixed last inconsistencies with "@" in feed generation

--- server/src/main/java/edu/byu/cs/tweeter/server/dao/StatusDAO.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/dao/StatusDAO.java
@@ -22,7 +22,7 @@ public class StatusDAO {
     public FeedResponse getFeed(FeedRequest request) {
         //TODO: Implement actual functionality once Databases are implemented
         String FEMALE_IMAGE_URL = "https://i.imgur.com/LiVHSFn.png";
-        return new FeedResponse(StatusGenerator.getNStatuses(request.getLimit(), new User("fname1", "lname1", "@tempAlias1", FEMALE_IMAGE_URL)), true);
+        return new FeedResponse(StatusGenerator.getNStatuses(request.getLimit(), new User("fname1", "lname1", "tempAlias1", FEMALE_IMAGE_URL)), true);
     }
 
     public StoryResponse getStory(StoryRequest request) {
